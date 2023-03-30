90c9700653604b365537d26248de763ee0273ed4
Tambet Matiisen <tambet.matiisen@ut.ee>
2023-03-30 17:13:35 +0300

    Changed lidar topic to more common one, bag is always looped, Timer class for quick timing

--- nodes/platform/simulation/obstacle_simulation.py
+++ nodes/platform/simulation/obstacle_simulation.py
@@ -1,48 +1,20 @@
-<<<<<<< HEAD
-#!/usr/bin/env python
-
-import rospy
-import threading
-<<<<<<< HEAD
-
-from geometry_msgs.msg import PointStamped, Point32
-from autoware_msgs.msg import DetectedObjectArray, DetectedObject
-from std_msgs.msg import ColorRGBA
-
-=======
-<<<<<<< HEAD
-=======
 #!/usr/bin/env python3
 
 import rospy
 import threading
->>>>>>> Added basic obstacle simulation node. Reorganize simulation and obstacle detection code a bit.
-import math
-import cv2
 
 from geometry_msgs.msg import PointStamped, Point32
 from autoware_msgs.msg import DetectedObjectArray, DetectedObject
-
 from std_msgs.msg import ColorRGBA
 
-from helpers import get_heading_from_pose_orientation
-
->>>>>>> Added basic obstacle simulation node. Reorganize simulation and obstacle detection code a bit.
 class ObstacleSimulation:
     def __init__(self):
         # get parameters
         self.publish_rate = rospy.get_param("~publish_rate", 10)
-<<<<<<< HEAD
 
         # list of objects
         self.objects = []
         self.id = 0
-=======
-        self.add_or_move = rospy.get_param("~add_or_move", 'move')
-
-        # list of objects
-        self.objects = []
->>>>>>> Added basic obstacle simulation node. Reorganize simulation and obstacle detection code a bit.
 
         # detected objects publisher
         self.objects_pub = rospy.Publisher('detected_objects', DetectedObjectArray, queue_size=10)
@@ -53,7 +25,6 @@ class ObstacleSimulation:
         rospy.loginfo("obstacle_simulation - initialized")
 
     def point_callback(self, msg):
-<<<<<<< HEAD
         # check if clicked on an existing object
         for o in self.objects:
             if o.pose.position.x - o.dimensions.x / 2.0 <= msg.point.x <= o.pose.position.x + o.dimensions.x / 2.0 and \
@@ -94,16 +65,6 @@ class ObstacleSimulation:
         self.objects.append(obj)
         rospy.loginfo("obstacle_simulation - added obstacle %d at (%f, %f, %f) in %s frame", self.id, msg.point.x, msg.point.y, msg.point.z, msg.header.frame_id)
         self.id += 1
-=======
-        if self.add_or_move == 'add':
-            self.objects.append(msg)
-            rospy.loginfo("obstacle_simulation - appended obstacle at (%f, %f, %f) in %s frame", msg.point.x, msg.point.y, msg.point.z, msg.header.frame_id)
-        elif self.add_or_move == 'move':
-            self.objects = [msg]
-            rospy.loginfo("obstacle_simulation - moved obstacle to (%f, %f, %f) in %s frame", msg.point.x, msg.point.y, msg.point.z, msg.header.frame_id)
-        else:
-            assert False, "Unknown add_or_move parameter value: " + self.add_or_move
->>>>>>> Added basic obstacle simulation node. Reorganize simulation and obstacle detection code a bit.
 
     def run(self):
         # start separate thread for spinning subcribers
@@ -119,7 +80,6 @@ class ObstacleSimulation:
             rate.sleep()
 
     def publish_detected_objects(self):
-<<<<<<< HEAD
         stamp = rospy.Time.now()
 
         # create message
@@ -134,48 +94,7 @@ class ObstacleSimulation:
         # overwrite object timestamp
         for o in msg.objects:
             o.header.stamp = stamp
-        
 
-=======
-        # create message
-        msg = DetectedObjectArray()
-        msg.header.stamp = rospy.Time.now()
-
-        # create objects
-        for i, o in enumerate(self.objects):
-            obj = DetectedObject()
-            obj.header.stamp = msg.header.stamp
-            obj.header.frame_id = o.header.frame_id
-            msg.header.frame_id = o.header.frame_id
-
-            obj.id = i
-            obj.label = 'unknown'
-            obj.color = ColorRGBA(1.0, 1.0, 1.0, 0.8)
-            obj.valid = True
-
-            obj.space_frame = o.header.frame_id
-            obj.pose.position.x = o.point.x
-            obj.pose.position.y = o.point.y
-            obj.pose.position.z = o.point.z
-            obj.pose.orientation.x = 0.0
-            obj.pose.orientation.y = 0.0
-            obj.pose.orientation.z = 0.0
-            obj.pose.orientation.w = 1.0
-            obj.dimensions.x = 1.0
-            obj.dimensions.y = 1.0
-            obj.dimensions.z = 1.0
-            obj.pose_reliable = True
-
-            points = cv2.boxPoints((
-                (obj.pose.position.x, obj.pose.position.y), 
-                (obj.dimensions.x, obj.dimensions.y), 
-                math.degrees(get_heading_from_pose_orientation(obj.pose))
-            ))
-            obj.convex_hull.polygon.points = [Point32(x, y, o.point.z) for x, y in points]
-
-            msg.objects.append(obj)
-        
->>>>>>> Added basic obstacle simulation node. Reorganize simulation and obstacle detection code a bit.
         self.objects_pub.publish(msg)
 
 if __name__ == '__main__':
---
+++ src/helpers/timer.py
@@ -0,0 +1,19 @@
+import time
+from collections import defaultdict
+
+class Timer:
+    def __init__(self):
+        self.dict = defaultdict(float)
+        self.reset()
+
+    def reset(self):
+        self.t = time.time()
+
+    def __call__(self, key):
+        t = time.time()
+        self.dict[key] += t - self.t
+        self.t = t
+        return self.dict[key]
+
+    def __str__(self):
+        return str(self.dict)
