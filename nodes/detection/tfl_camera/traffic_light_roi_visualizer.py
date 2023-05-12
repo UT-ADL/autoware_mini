f5b3129dccb9a802580e7d69e5f06ab08c6e901d
Edgar Sepp <edgar.sepp@ut.ee>
2023-05-13 01:16:50 +0300

    cleaning, added batch prediction and removed subscription to camera_info

--- nodes/detection/tfl_camera/traffic_light_roi_visualizer.py
+++ nodes/detection/tfl_camera/traffic_light_roi_visualizer.py
@@ -25,17 +25,16 @@ class TrafficLightRoiVisualizer:
         # Subscribers
         signals_sub = message_filters.Subscriber('signals', Signals)
         camera_img_sub = message_filters.Subscriber('/camera_fl/image_raw', Image)
-        camera_info_sub = message_filters.Subscriber('/camera_fl/camera_info', CameraInfo)
         # TODO: add sub to results of the classifier
 
         # Synchronizer
-        ts = message_filters.ApproximateTimeSynchronizer([signals_sub, camera_img_sub, camera_info_sub], queue_size = 10, slop = 0.2)
+        ts = message_filters.ApproximateTimeSynchronizer([signals_sub, camera_img_sub], queue_size = 10, slop = 0.2)
         ts.registerCallback(self.data_callback)
 
 
         self.bridge = CvBridge()
 
-    def data_callback(self, signals_msg, camera_img_msg, camera_info_msg):
+    def data_callback(self, signals_msg, camera_img_msg):
 
         try:
             image = self.bridge.imgmsg_to_cv2(camera_img_msg,  desired_encoding='rgb8')
@@ -44,11 +43,10 @@ class TrafficLightRoiVisualizer:
 
         if len(signals_msg.Signals) > 0:
 
-            rois = convert_signals_to_rois(signals_msg.Signals, camera_info_msg.width, camera_info_msg.height, self.radius_to_roi_factor)
-
+            rois = convert_signals_to_rois(signals_msg.Signals, image.shape[1], image.shape[0], self.radius_to_roi_factor)
             for roi in rois:
-                start_point = (int(roi[2]), int(roi[4]))
-                end_point = (int(roi[3]), int(roi[5]))
+                start_point = (int(roi[2]), int(roi[4]))    # top left
+                end_point = (int(roi[3]), int(roi[5]))      # bottom right
                 # TODO use classifier results to set color and label for roi
                 color = (0, 255, 0)
                 thickness = 2
