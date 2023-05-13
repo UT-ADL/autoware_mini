08724c787654269acaa028ae633e2b3b0f04ed13
Tambet Matiisen <tambet.matiisen@ut.ee>
2023-05-13 07:04:26 +0300

    Switched back to tf2_ros.

--- nodes/platform/simulation/bicycle_simulation.py
+++ nodes/platform/simulation/bicycle_simulation.py
@@ -141,13 +141,17 @@ class BicycleSimulation:
 
     def publish_base_link_to_map_tf(self, stamp):
             
-        self.br.sendTransform(
-            (self.x, self.y, 0.0),
-            (self.orientation.x, self.orientation.y, self.orientation.z, self.orientation.w),
-            stamp,
-            "base_link",
-            "map"
-        )
+        t = TransformStamped()
+
+        t.header.stamp = stamp
+        t.header.frame_id = "map"
+        t.child_frame_id = "base_link"
+
+        t.transform.translation.x = self.x
+        t.transform.translation.y = self.y
+        t.transform.rotation = self.orientation
+
+        self.br.sendTransform(t)
 
     def publish_current_pose(self, stamp):
 
