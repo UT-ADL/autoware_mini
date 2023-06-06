9d4c391cd93fa4c10d7ee7c894692fe55df35ae6
Tambet Matiisen <tambet.matiisen@ut.ee>
2023-06-06 21:24:55 +0300

    Initial version of EMA tracker

--- src/helpers/detection.py
+++ src/helpers/detection.py
@@ -1,5 +1,6 @@
 import math
 import cv2
+import numpy as np
 
 from geometry_msgs.msg import PolygonStamped, Point
 
@@ -31,3 +32,43 @@ def create_hull(obj_pose, obj_dims, output_frame, stamp):
     convex_hull.polygon.points = [Point(x, y, obj_pose.position.z) for x, y in points]
 
     return convex_hull
+
+def calculate_iou(boxes1, boxes2):
+    """
+    Calculate the IOU between two sets of bounding boxes.
+
+    Args:
+        boxes1: a numpy array of shape (n, 4) containing the coordinates of n bounding boxes in the format (x1, y1, x2, y2).
+        boxes2: a numpy array of shape (m, 4) containing the coordinates of m bounding boxes in the format (x1, y1, x2, y2).
+
+    Returns:
+        a numpy array of shape (n, m) containing the IOU between all pairs of bounding boxes.
+    """
+    # Calculate the area of each bounding box
+    area1 = (boxes1[:, 2] - boxes1[:, 0]) * (boxes1[:, 3] - boxes1[:, 1])
+    area2 = (boxes2[:, 2] - boxes2[:, 0]) * (boxes2[:, 3] - boxes2[:, 1])
+
+    # Calculate the coordinates of the intersection bounding boxes
+    intersection_x1 = np.maximum(boxes1[:, 0][:, np.newaxis], boxes2[:, 0])
+    intersection_y1 = np.maximum(boxes1[:, 1][:, np.newaxis], boxes2[:, 1])
+    intersection_x2 = np.minimum(boxes1[:, 2][:, np.newaxis], boxes2[:, 2])
+    intersection_y2 = np.minimum(boxes1[:, 3][:, np.newaxis], boxes2[:, 3])
+
+    # Calculate the area of the intersection bounding boxes
+    intersection_area = np.maximum(intersection_x2 - intersection_x1, 0) * np.maximum(intersection_y2 - intersection_y1, 0)
+
+    # Calculate the union of the bounding boxes
+    union_area = area1[:, np.newaxis] + area2 - intersection_area
+
+    # Calculate the IOU
+    iou = intersection_area / union_area
+
+    return iou
+
+
+if __name__ == '__main__':
+    boxes1 = np.array([[0, 0, 10, 10], [10, 10, 20, 20]])
+    boxes2 = np.array([[5, 5, 15, 15], [15, 15, 25, 25]])
+
+    iou = calculate_iou(boxes1, boxes2)
+    assert np.allclose(iou, np.array([[25 / 175, 0.0], [25 / 175, 25 / 175]])), str(iou)
