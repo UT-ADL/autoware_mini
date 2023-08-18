ff3ba782a95a11c5e8dfe06fc571a2a33ab3df05
Edgar Sepp <edgar.sepp@ut.ee>
2023-08-18 18:14:38 +0300

    [#15] added animated wheel, blinkers, relocated, changed background and styling

--- nodes/localization/gnss/novatel_oem7_visualizer.py
+++ nodes/localization/gnss/novatel_oem7_visualizer.py
@@ -54,10 +54,10 @@ BESTPOS_POS_TYPE = {
     80: "INS_PPP_BASIC"
     }
 
-GREEN = ColorRGBA(0.0, 1.0, 0.0, 0.3)
-YELLOW = ColorRGBA(1.0, 1.0, 0.0, 0.3)
-RED = ColorRGBA(1.0, 0.0, 0.0, 0.3)
-
+BLACK = ColorRGBA(0.0, 0.0, 0.0, 0.8)
+GREEN = ColorRGBA(0.0, 1.0, 0.0, 1.0)
+YELLOW = ColorRGBA(1.0, 1.0, 0.0, 1.0)
+RED = ColorRGBA(1.0, 0.0, 0.0, 1.0)
 WHITE = ColorRGBA(1.0, 1.0, 1.0, 1.0)
 
 INS_SOLUTION_GOOD = 3
@@ -86,8 +86,8 @@ class NovatelOem7Visualizer:
         rospy.Subscriber('/novatel/oem7/inspva', INSPVA, self.inspva_callback, queue_size=1)
         rospy.Subscriber('/novatel/oem7/bestpos', BESTPOS, self.bestpos_callback, queue_size=1)
 
-        # Parameters
-        self.global_top = 500
+        # Internal parameters
+        self.global_top = 595
         self.global_left = 10
         self.global_width = 250
         self.global_height = 17
@@ -96,9 +96,9 @@ class NovatelOem7Visualizer:
 
     def inspva_callback(self, msg):
 
-        bg_color = GREEN
+        fg_color = WHITE
         if msg.status.status != INS_SOLUTION_GOOD:
-            bg_color = YELLOW
+            fg_color = YELLOW
 
         if msg.status.status not in INSPVA_STATUS:
             text = "Unknown IMU status: {}".format(msg.status.status)
@@ -112,17 +112,17 @@ class NovatelOem7Visualizer:
         inspva_status.width = self.global_width
         inspva_status.height = self.global_height
         inspva_status.text_size = self.text_size
-        inspva_status.fg_color = WHITE
-        inspva_status.bg_color = bg_color
+        inspva_status.fg_color = fg_color
+        inspva_status.bg_color = BLACK
 
         self.inspva_status_pub.publish(inspva_status)
 
     def bestpos_callback(self, msg):
         
         ################# bestpos_pos_type
-        bg_color = GREEN
+        fg_color = WHITE
         if msg.pos_type.type != INS_RTKFIXED:
-            bg_color = YELLOW
+            fg_color = YELLOW
 
         if msg.pos_type.type not in BESTPOS_POS_TYPE:
             text = "Unknown BESTPOS pos_type: {}".format(msg.pos_type.type)
@@ -136,17 +136,17 @@ class NovatelOem7Visualizer:
         bestpos_pos_type.width = self.global_width
         bestpos_pos_type.height = self.global_height
         bestpos_pos_type.text_size = self.text_size
-        bestpos_pos_type.fg_color = WHITE
-        bestpos_pos_type.bg_color = bg_color
+        bestpos_pos_type.fg_color = fg_color
+        bestpos_pos_type.bg_color = BLACK
 
         self.bestpos_pos_type_pub.publish(bestpos_pos_type)
 
         ################# num_sol_svs
-        bg_color = GREEN
+        fg_color = WHITE
         if msg.num_sol_svs < self.number_of_satellites_good:
-            bg_color = YELLOW
+            fg_color = YELLOW
         if msg.num_sol_svs < self.number_of_satellites_bad:
-            bg_color = RED
+            fg_color = RED
 
         num_sol_svs = OverlayText()
         num_sol_svs.text = "Num. satellites: {}".format(msg.num_sol_svs)
@@ -155,19 +155,19 @@ class NovatelOem7Visualizer:
         num_sol_svs.width = self.global_width
         num_sol_svs.height = self.global_height
         num_sol_svs.text_size = self.text_size
-        num_sol_svs.fg_color = WHITE
-        num_sol_svs.bg_color = bg_color
+        num_sol_svs.fg_color = fg_color
+        num_sol_svs.bg_color = BLACK
 
         self.bestpos_num_sol_svs_pub.publish(num_sol_svs)
 
         ################# loc_stdev
         location_stdev = math.sqrt(msg.lat_stdev**2 + msg.lon_stdev**2)
 
-        bg_color = GREEN
+        fg_color = WHITE
         if location_stdev > self.location_accuracy_stdev_good:
-            bg_color = YELLOW
+            fg_color = YELLOW
         if location_stdev > self.location_accuracy_stdev_bad:
-            bg_color = RED
+            fg_color = RED
 
         loc_stdev = OverlayText()
         loc_stdev.text = "Location stdev: {:.2f} m".format(location_stdev)
@@ -176,17 +176,17 @@ class NovatelOem7Visualizer:
         loc_stdev.width = self.global_width
         loc_stdev.height = self.global_height
         loc_stdev.text_size = self.text_size
-        loc_stdev.fg_color = WHITE
-        loc_stdev.bg_color = bg_color
+        loc_stdev.fg_color = fg_color
+        loc_stdev.bg_color = BLACK
 
         self.bestpos_loc_stdev_pub.publish(loc_stdev)
 
         ################# diff_age
-        bg_color = GREEN
+        fg_color = WHITE
         if msg.diff_age > self.differential_age_good:
-            bg_color = YELLOW
+            fg_color = YELLOW
         if msg.diff_age > self.differential_age_bad:
-            bg_color = RED
+            fg_color = RED
 
         diff_age = OverlayText()
         diff_age.text = "Differential age: {:.2f} s".format(msg.diff_age)
@@ -195,8 +195,8 @@ class NovatelOem7Visualizer:
         diff_age.width = self.global_width
         diff_age.height = self.global_height
         diff_age.text_size = self.text_size
-        diff_age.fg_color = WHITE
-        diff_age.bg_color = bg_color
+        diff_age.fg_color = fg_color
+        diff_age.bg_color = BLACK
 
         self.bestpos_diff_age_pub.publish(diff_age)
 
--- nodes/platform/lexus/pacmod_state_visualizer.py
+++ nodes/platform/lexus/pacmod_state_visualizer.py
@@ -6,12 +6,17 @@ from std_msgs.msg import ColorRGBA, Bool
 from automotive_navigation_msgs.msg import ModuleState
 from pacmod3_msgs.msg import AllSystemStatuses
 from jsk_rviz_plugins.msg import OverlayText
+from sensor_msgs.msg import Image
+from autoware_msgs.msg import VehicleCmd
+import cv2
+from cv_bridge import CvBridge
 
-BLUE = ColorRGBA(0.6, 0.4, 1.0, 0.9)
-GREEN = ColorRGBA(0.2, 1.0, 0.2, 0.9)
-YELLOW = ColorRGBA(1.0, 1.0, 0.0, 0.9)
-RED = ColorRGBA(1.0, 0.0, 0.0, 0.9)
-BLACK = ColorRGBA(0.0, 0.0, 0.0, 0.5)
+
+BLUE = ColorRGBA(0.4, 0.6, 1.0, 1.0)
+GREEN = ColorRGBA(0.0, 1.0, 0.0, 1.0)
+YELLOW = ColorRGBA(1.0, 1.0, 0.0, 1.0)
+RED = ColorRGBA(1.0, 0.0, 0.0, 1.0)
+BLACK = ColorRGBA(0.0, 0.0, 0.0, 0.8)
 WHITE = ColorRGBA(1.0, 1.0, 1.0, 1.0)
 
 # Ket values in /pacmod/all_system_statuses
@@ -23,32 +28,46 @@ TURN_SIGNALS = 7
 class PacmodStateVisualizer:
     def __init__(self):
 
+        # Parameters
+        self.image_path = rospy.get_param('~image_path')
+
         # Publishers
         self.pacmod_state_pub = rospy.Publisher('pacmod_state', OverlayText, queue_size=1)
         self.ssc_state_pub = rospy.Publisher('ssc_state', OverlayText, queue_size=1)
         self.autonomy_pub = rospy.Publisher('autonomy', OverlayText, queue_size=1)
+        self.steering_wheel_pub = rospy.Publisher('steering_wheel', Image, queue_size=1)
+        self.right_blinker_pub = rospy.Publisher('right_blinker', Image, queue_size=1)
+        self.left_blinker_pub =  rospy.Publisher('left_blinker', Image, queue_size=1)
 
         # Subscribers
         rospy.Subscriber('/ssc/module_states', ModuleState, self.ssc_module_states_callback, queue_size=1)
         rospy.Subscriber('/pacmod/all_system_statuses', AllSystemStatuses, self.pacmod_all_system_statuses, queue_size=1)
         rospy.Subscriber('/pacmod/enabled', Bool, self.pacmod_enabled, queue_size=1)
+        rospy.Subscriber('/control/vehicle_cmd', VehicleCmd, self.vehicle_cmd_callback, queue_size=1)
 
-        # Parameters
-        self.global_top = 200
+        # Internal parameters
+        self.global_top = 330
         self.global_left = 10
         self.global_width = 250
 
         self.ssc_states={}
+        self.bridge = CvBridge()
+        self.is_autonomous = False
+        
 
     def pacmod_enabled(self, msg):
+
+        self.is_autonomous = msg.data
+        autonomous = self.is_autonomous
 
         enabled = OverlayText()
         enabled.top = self.global_top
         enabled.left = self.global_left
         enabled.width = self.global_width
-        enabled.height = 40
-        enabled.text_size = 20
-        if msg.data:
+        enabled.height = 35
+        enabled.text_size = 22
+        enabled.bg_color = BLACK
+        if autonomous:
             enabled.text = "AUTONOMOUS"
             enabled.fg_color = BLUE
         else:
@@ -58,23 +77,96 @@ class PacmodStateVisualizer:
         self.autonomy_pub.publish(enabled)
 
 
+    def vehicle_cmd_callback(self, msg):
+
+        autonomous = self.is_autonomous
+
+        # Wheel animation
+        wheel_img = cv2.imread(self.image_path + "wheel_s.png")
+        if wheel_img is not None:
+
+            height, width = wheel_img.shape[:2]
+            # TODO: covert correctly to steering wheel angle
+            angle = math.degrees(msg.ctrl_cmd.steering_angle) * 10
+            rotation_matrix = cv2.getRotationMatrix2D((width / 2, height / 2), angle, 1)
+            rotated_wheel_img = cv2.warpAffine(wheel_img, rotation_matrix, (width, height))
+
+            try:
+                if autonomous:
+                    # make Blue channel 255, convert to blueish color
+                    rotated_wheel_img[:, :, 0] *= 255
+                    rotated_wheel_img[:, :, 1] *= 150
+                    rotated_wheel_img[:, :, 2] *= 100
+                else:
+                    # make green channel 255
+                    rotated_wheel_img[:, :, 1] *= 255
+
+                wheel_msg = self.bridge.cv2_to_imgmsg(rotated_wheel_img, encoding="bgr8")
+                self.steering_wheel_pub.publish(wheel_msg)
+            except Exception as e:
+                rospy.logerr("Dashboard: Error publishing wheel image: {}".format(str(e)))
+        else:
+            rospy.logerr("Dashboard: Wheel image not found or couldn't be loaded")
+
+
+        # Blinkers
+        right_img = cv2.imread(self.image_path + "right.png")
+
+        if right_img is not None:
+            try:
+                if msg.lamp_cmd.r == 1:
+                    # make Yellow
+                    right_img[:, :, 1] *= 255
+                    right_img[:, :, 2] *= 255
+
+                right_msg = self.bridge.cv2_to_imgmsg(right_img, encoding="bgr8")
+                self.right_blinker_pub.publish(right_msg)
+            except Exception as e:
+                rospy.logerr("Dashboard: Error publishing right blinker image: {}".format(str(e)))
+        else:
+            rospy.logerr("Dashboard: Right blinker image not found or couldn't be loaded")
+
+
+        left_img = cv2.imread(self.image_path + "left.png")
+
+        if left_img is not None:
+            try:
+                if msg.lamp_cmd.l == 1:
+                    # make Yellow
+                    left_img[:, :, 1] *= 255
+                    left_img[:, :, 2] *= 255
+
+                left_msg = self.bridge.cv2_to_imgmsg(left_img, encoding="bgr8")
+                self.left_blinker_pub.publish(left_msg)
+            except Exception as e:
+                rospy.logerr("Dashboard: Error publishing left blinker image: {}".format(str(e)))
+        else:
+            rospy.logerr("Dashboard: Left blinker image not found or couldn't be loaded")
+
+
     def ssc_module_states_callback(self, msg):
 
         # collect all the latest states of individual modules
-        self.ssc_states[msg.name] = msg.state # + " " + msg.info
+        self.ssc_states[msg.name] = msg.state + " " + msg.info
 
         ssc_status_text = ""
         for key in self.ssc_states:
             ssc_status_text += key + ": " + self.ssc_states[key] + "\n"
 
+        fg_color = WHITE
+        if 'failure' in ssc_status_text:
+            fg_color = YELLOW
+        if 'fatal' in ssc_status_text:
+            fg_color = RED
+
         ssc_state = OverlayText()
-        ssc_state.top = self.global_top + 50
+        ssc_state.top = self.global_top + 115
         ssc_state.left = self.global_left
         ssc_state.width = self.global_width
-        ssc_state.height = 50
+        ssc_state.height = 70
         ssc_state.text_size = 10
         ssc_state.text = ssc_status_text
-        ssc_state.fg_color = WHITE
+        ssc_state.fg_color = fg_color
         ssc_state.bg_color = BLACK
 
         self.ssc_state_pub.publish(ssc_state)
@@ -88,14 +180,20 @@ class PacmodStateVisualizer:
         steering_state = self.get_state_string(STEERING, msg)
         turn_signals_state = self.get_state_string(TURN_SIGNALS, msg)
 
+        fg_color = WHITE
+        if 'Overridden' in accelerator_state:
+            fg_color = YELLOW
+        if 'Fault' in accelerator_state:
+            fg_color = RED
+
         pacmod_state = OverlayText()
-        pacmod_state.top = self.global_top + 100
+        pacmod_state.top = self.global_top + 185
         pacmod_state.left = self.global_left
         pacmod_state.width = self.global_width
-        pacmod_state.height = 60
+        pacmod_state.height = 80
         pacmod_state.text_size = 10
         pacmod_state.text = "Accelerator:" + accelerator_state + "\nBrakes:" + brakes_state + "\nSteering:" + steering_state + "\nTurn signals:" + turn_signals_state
-        pacmod_state.fg_color = WHITE
+        pacmod_state.fg_color = fg_color
         pacmod_state.bg_color = BLACK
 
         self.pacmod_state_pub.publish(pacmod_state)
